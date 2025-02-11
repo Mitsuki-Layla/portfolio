11de7492176eb7135a865739e14b1d49d9a8b505
Mitsuki-Layla <mitsukilayla.design@gmail.com>
2025-02-11 13:59:10 +0100

    upload finished project

---
+++ main.js
@@ -0,0 +1,14 @@
+
+
+//smooth scrolling for the links in the same page
+
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
